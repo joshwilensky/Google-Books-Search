8a0c0b3e0768cbdad349c7e0b01a318d04538c6f
Joshua Wilensky <joshwilensky@gmail.com>
2020-09-04 16:12:18 -0400

    Added socket.io

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
-
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
+io.on('connection', socket => {
+  socket.emit('request', "You've entered the book"); // emit an event to the socket
+  io.emit('broadcast', "Now Broadcasting"); // emit an event to all connected sockets
+  socket.on('reply', () => { "Now Replying" }); // listen to the event
+});
 // Configure body parsing for AJAX requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
